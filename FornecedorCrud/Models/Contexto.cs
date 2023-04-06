32eb13edf3d1e5a04643f44798bace709fbd76fa
Gustavo Guilherme <gustavo.gsilva1723@gmail.com>
2023-04-05 23:08:05 -0300

    Adicionar arquivos de projeto.

---
+++ FornecedorCrud/Models/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FornecedorCrud.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Fornecedor> Fornecedor { get; set; } 
+    }
+}
---
+++ FornecedorCrud/Models/Fornecedor.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FornecedorCrud.Models
+{
+    [Table("Fornecedor")]
+    public class Fornecedor
+    {
+
+        [Column("Id")]
+        [Display(Name = "Codigo")]
+        public int FornecedorId { get; set; }
+
+        [Column("Fornecedor_Cnpj")]
+        [Display(Name = "Cnpj")]
+        public string? FornecedorCnpj { get; set; }
+
+        [Column("Fornecedor_Email")]
+        [Display(Name = "Email")]
+        public string? FornecedorEmail { get; set;}
+
+        [Column("Fornecedor_Senha")]
+        [Display(Name = "Senha")]
+        public string? FornecedorSenha { get; set; }
+
+        [Column("Fornecedor_Nome")]
+        [Display(Name = "Nome")]
+        public string? FornecedorName { get; set;}
+    }
+}
